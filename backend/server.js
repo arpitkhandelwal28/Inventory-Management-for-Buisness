4678f30a221405528933fb4b3263ade1b31d51ff
Arpit Khandelwal <AKhandelwal1228@gmail.com>
2025-06-15 17:48:32 +0530

    some link

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 const port = process.env.PORT || 5000 ;
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://inventory-management-for-buisness-2.onrender.com",
     credentials: true,
 }));
 
